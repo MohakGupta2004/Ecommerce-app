50190d59bd11c865a9fe7d33734cf502aad0066a
mohak <mohakgupta500@gmail.com>
2025-08-24 22:01:46 +0530

    code: catch all routes add

--- src/app.js
+++ src/app.js
@@ -1,26 +1,28 @@
 import express from 'express'
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.routes.js'
+import { ApiResponse } from './utils/ApiResponse.js';
 const app = express();
 
 //middlewares
 app.use(express.json())
 app.use(cookieParser());
 app.use(express.urlencoded({
-    extended: true,
+  extended: true,
 }))
 
 //routes
 app.use("/api/v1/auth", authRouter)
 
 
-
-
-
-
-app.get("/healthcheck", (req, res)=>{
+app.get("/healthcheck", (req, res) => {
   return res.json(new ApiResponse(200, "server running"))
-} )
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  return res.json(new ApiResponse(err.statusCode || 500, err.message || "Something went wrong"))
+})
 
 export default app
 
